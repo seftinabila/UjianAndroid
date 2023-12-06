d750443b6c365737053541eb0431c42e1ea80cc1
seftinabila <seftina808@gmail.com>
2023-12-06 20:15:15 +0700

    UTS Membuat perulangan 1-20, mengskip bilangan ganjil, dan menambahkan angka index

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -2,9 +2,7 @@ package com.example.ujianandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,17 +15,24 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ListView lvNama = (ListView) findViewById(R.id.lvNama);
+        ListView lvNama = findViewById(R.id.lvNama);
 
         ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
 
-        if(daftar_nama.isEmpty()){
+        if (daftar_nama.isEmpty()) {
             daftar_nama.add("data masih kosong");
+        }
+
+        // Menggabungkan urutan angka ke sebelah kiri dari nama
+        for (int i = 0; i < daftar_nama.size(); i++) {
+            String[] parts = daftar_nama.get(i).split(" ");
+            if (parts.length > 2) { // Memastikan ada nama dan angka
+                daftar_nama.set(i, parts[parts.length - 1] + " " + String.join(" ", parts));
+            }
         }
 
         ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
 
         lvNama.setAdapter(ad_nama);
-
     }
 }
--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -18,9 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edNamaDepan = (EditText) findViewById(R.id.edNamaDepan);
-        EditText edNamaBelakang = (EditText) findViewById(R.id.edNamaBelakang);
-        Button btnSimpan = (Button) findViewById(R.id.btnSimpan);
+        EditText edNamaDepan = findViewById(R.id.edNamaDepan);
+        EditText edNamaBelakang = findViewById(R.id.edNamaBelakang);
+        Button btnSimpan = findViewById(R.id.btnSimpan);
 
         ArrayList<String> daftar_nama = new ArrayList<>();
 
@@ -32,12 +32,17 @@ public class MainActivity extends AppCompatActivity {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
 
-                if(isian_nama_depan.isEmpty()){
+                if (isian_nama_depan.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
-                }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
-                    daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
+                } else {
+                    // Menghasilkan daftar nama dengan melewatkan bilangan genap dan menambahkan indeks
+                    for (int i = 1; i <= 20; i++) {
+                        if (i % 2 != 0) { // Skip bilangan genap
+                            String nama_lengkap = i + "." + isian_nama_depan + " " + isian_nama_belakang + " " ;
+                            daftar_nama.add(nama_lengkap);
+                        }
+                    }
+
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
